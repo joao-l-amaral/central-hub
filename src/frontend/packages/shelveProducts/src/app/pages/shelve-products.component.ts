f5143ef5e52789c91cebb6320c4fc2eeec01f32c
João Amaral <joaoamaral1989@hotmail.com>
2026-04-22 00:17:01 +0100

    INF-3 Add unit test, e2e test and dry-run build jobs

--- src/frontend/packages/shelveProducts/src/app/pages/shelve-products.component.ts
+++ src/frontend/packages/shelveProducts/src/app/pages/shelve-products.component.ts
@@ -1,7 +1,18 @@
-import { ChangeDetectionStrategy, Component, inject, OnDestroy, signal, ViewChild } from '@angular/core';
+import {
+    ChangeDetectionStrategy,
+    Component,
+    inject,
+    OnDestroy,
+    signal,
+    ViewChild,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatButton, MatIconButton } from '@angular/material/button';
-import { MatSidenav, MatSidenavContainer, MatSidenavContent } from '@angular/material/sidenav';
+import { MatIconButton } from '@angular/material/button';
+import {
+    MatSidenav,
+    MatSidenavContainer,
+    MatSidenavContent,
+} from '@angular/material/sidenav';
 import { FormComponent } from '../ui/form/form.component';
 import { MatIcon } from '@angular/material/icon';
 import { StatisticsPanelComponent } from '../ui/statistics-panel/statistics-panel.component';
@@ -14,11 +25,10 @@ import { ToastrService } from 'ngx-toastr';
 import { MatDialog } from '@angular/material/dialog';
 import { Subscription } from 'rxjs';
 import {
+    ConfirmationModalComponent,
     I18nService,
     InternalizationPipe,
     MF_FRONTEND,
-    ConfirmationModalComponent,
-    LibCustomButtonDirective
 } from '@portal/library';
 import { SelectionModel } from '@angular/cdk/collections';
 import { ShelveProduct } from '../interface/shelve-product.interface';
@@ -38,8 +48,6 @@ import { ShelveProduct } from '../interface/shelve-product.interface';
         MatSidenavContent,
         StatisticsPanelComponent,
         TableComponent,
-        LibCustomButtonDirective,
-        MatButton
     ],
     standalone: true,
     changeDetection: ChangeDetectionStrategy.OnPush,
@@ -48,10 +56,10 @@ import { ShelveProduct } from '../interface/shelve-product.interface';
         ShelveProductService,
         SideNavService,
         StatisticsPanelService,
-        { provide: MF_FRONTEND, useValue: 'shelveProducts' }
-    ]
+        { provide: MF_FRONTEND, useValue: 'shelveProducts' },
+    ],
 })
-export class ShelveProductsComponent  implements OnDestroy {
+export class ShelveProductsComponent implements OnDestroy {
     readonly #statisticsPanelService = inject(StatisticsPanelService);
     readonly #tableService = inject(TableService);
     readonly #sideNavService = inject(SideNavService);
@@ -69,41 +77,45 @@ export class ShelveProductsComponent  implements OnDestroy {
     private readonly subClose: Subscription;
     private readonly subChanged: Subscription;
 
-    private readonly defaultShelveProduct = this.#sideNavService.defaultShelveProduct;
+    private readonly defaultShelveProduct =
+        this.#sideNavService.defaultShelveProduct;
 
     readonly deletionDisable = signal<boolean>(true);
 
-
     constructor() {
-
-        console.info("--- ShelveProductsComponent initialized ---");
+        console.info('--- ShelveProductsComponent initialized ---');
         this.sub = this.#sideNavService.toggleSidenavSource.subscribe(() => {
-            if(!this.sidenav.opened) {
+            if (!this.sidenav.opened) {
                 this.sidenav.toggle();
             }
         });
-        this.subClose = this.#sideNavService.toggleSidenavClose.subscribe(() => {
-            if(this.sidenav.opened) {
-                this.sidenav.close();
+        this.subClose = this.#sideNavService.toggleSidenavClose.subscribe(
+            () => {
+                if (this.sidenav.opened) {
+                    this.sidenav.close();
+                }
             }
-        });
-        this.subChanged = this.#tableService.selection().changed.subscribe(() => {
-            const hasValue = !this.#tableService.selection().hasValue();
-            this.deletionDisable.set(hasValue);
-        });
+        );
+        this.subChanged = this.#tableService
+            .selection()
+            .changed.subscribe(() => {
+                const hasValue = !this.#tableService.selection().hasValue();
+                this.deletionDisable.set(hasValue);
+            });
     }
 
     protected async refreshProducts() {
         this.#tableService.isLoadingResults.set(true);
-        const shelveProducts = await this.#shelveProductService.getShelveProduct();
+        const shelveProducts =
+            await this.#shelveProductService.getShelveProduct();
         this.#tableService.dataSource.set(shelveProducts);
         this.#tableService.isLoadingResults.set(false);
     }
 
-    protected addProduct(){
+    protected addProduct() {
         this.#sideNavService.isEditMode.set(false);
         this.#sideNavService.productSelected.set(this.defaultShelveProduct);
-        if(!this.sidenav.opened) {
+        if (!this.sidenav.opened) {
             this.sidenav.toggle();
         } else {
             this.sidenav.close();
@@ -114,31 +126,54 @@ export class ShelveProductsComponent  implements OnDestroy {
     protected removeProducts() {
         const selectedProducts = this.#tableService.selection().selected;
 
-        const modalMsg = (selectedProducts?.length > 1) ?
-            this.#i18nService.translate(this.#mf, "remove.multi.product", selectedProducts.map(product => product.shelveCode).join(', ')) :
-            this.#i18nService.translate(this.#mf, "remove.one.product");
+        const modalMsg =
+            selectedProducts?.length > 1
+                ? this.#i18nService.translate(
+                      this.#mf,
+                      'remove.multi.product',
+                      selectedProducts
+                          .map((product) => product.shelveCode)
+                          .join(', ')
+                  )
+                : this.#i18nService.translate(this.#mf, 'remove.one.product');
 
         const dialogRef = this.#dialog.open(ConfirmationModalComponent, {
-            data: {title: this.#i18nService.translate(this.#mf, "modal.title"), message: modalMsg},
+            data: {
+                title: this.#i18nService.translate(this.#mf, 'modal.title'),
+                message: modalMsg,
+            },
         });
 
-        dialogRef.afterClosed().subscribe(result => {
+        dialogRef.afterClosed().subscribe((result) => {
             if (result !== undefined) {
                 for (const product of selectedProducts) {
-                    this.#shelveProductService.removeProduct(product.shelveCode).then(
-                        () => {
+                    this.#shelveProductService
+                        .removeProduct(product.shelveCode)
+                        .then(() => {
                             this.#statisticsPanelService.getStatistics();
-                            this.#tableService.dataSource.set(this.#tableService.dataSource().filter(shelveProduct => shelveProduct.shelveCode !== product.shelveCode));
-                            this.#toastr.success(`Product "${product.shelveCode}" was removed.`, '', {
-                                positionClass: 'toast-bottom-left'
-                            });
-                            this.#tableService.selection.set(new SelectionModel<ShelveProduct>(true, []));
-                        }
-                    )
+                            this.#tableService.dataSource.set(
+                                this.#tableService
+                                    .dataSource()
+                                    .filter(
+                                        (shelveProduct) =>
+                                            shelveProduct.shelveCode !==
+                                            product.shelveCode
+                                    )
+                            );
+                            this.#toastr.success(
+                                `Product "${product.shelveCode}" was removed.`,
+                                '',
+                                {
+                                    positionClass: 'toast-bottom-left',
+                                }
+                            );
+                            this.#tableService.selection.set(
+                                new SelectionModel<ShelveProduct>(true, [])
+                            );
+                        });
                 }
             }
         });
-
     }
 
     ngOnDestroy(): void {
@@ -148,7 +183,7 @@ export class ShelveProductsComponent  implements OnDestroy {
     }
 
     closeNav() {
-        this.sidenav.toggle()
+        this.sidenav.toggle();
         this.#sideNavService.productSelected.set(this.defaultShelveProduct);
     }
 }
