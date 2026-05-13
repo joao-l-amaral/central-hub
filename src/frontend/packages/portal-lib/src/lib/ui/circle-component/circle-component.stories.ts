417b9a973105b772bbbe647f6041c46c24a875e4
João Amaral <joaoamaral1989@hotmail.com>
2026-05-13 23:10:38 +0100

    Fix storybook

--- src/frontend/packages/portal-lib/src/lib/ui/circle-component/circle-component.stories.ts
+++ src/frontend/packages/portal-lib/src/lib/ui/circle-component/circle-component.stories.ts
@@ -13,17 +13,23 @@ export const Circle: Story = {
     render: (args) => ({
         props: {
             ...args,
-            action: () => args.selected && alert('Circle action triggered with selected=true!'),
+            onClicked: (value: string) => {
+                alert(`Circle clicked: ${value}`);
+            },
         },
         template: `
       <div style="width: 100px; height: 100px; display: flex; align-items: center; justify-content: center;">
         <div style="width: 100%; height: 100%">
-          <lib-circle-component ${argsToTemplate(args)} [onAction]="action" />
+          <lib-circle-component
+            ${argsToTemplate(args)}
+            (clicked)="onClicked($event)"
+          ></lib-circle-component>
         </div>
       </div>
     `,
     }),
     args: {
+        id: 'batatas',
         icon: 'bi bi-playstation',
         label: 'Playstation',
         selected: true,
