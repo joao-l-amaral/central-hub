93161fe13c3c8d54cd32ca72c919baa94f36bbfc
João Amaral <joaoamaral1989@hotmail.com>
2026-06-24 22:48:30 +0100

    Create SideNav without style + fix founded bugs related with gameQ microfrontend

--- src/frontend/packages/portal-lib/src/lib/ui/index.ts
+++ src/frontend/packages/portal-lib/src/lib/ui/index.ts
@@ -3,5 +3,6 @@ export * from './button/button';
 export * from './circle-component/circle-component';
 export * from './confirmation-modal/confirmation-modal.component';
 export * from './keyValue/keyValue.component';
-export * from './loading-block/index';
 export * from './search-input/search-input';
+export * from './loading-block/index';
+export * from './sidebar-nav/index';
